00fb578a035ccb3f8390b44a319a80fddd76b27d
jtierney06 <jamespt06@icloud.com>
2025-01-15 14:51:12 -0500

    ,

--- Point.java
+++ Point.java
@@ -34,6 +34,13 @@ public class Point{
     }
 
     public boolean isCollinear(Point p1, Point p2){
-        
+        if(this.X == p1.X && this.X == p2.X){
+            return true
+        }
+
+        double slope1 = (double)(p1.Y - this.Y)/(p1.X - this.X);
+         double slope2 = (double)(p2.Y - this.Y)/(p2.X - this.X);
+         return Math.abs(slope1 - slope2) < 0.01;
+
     }
 }
--- TestPoint.java
+++ TestPoint.java
@@ -10,6 +10,13 @@ public class TestPoint {
         System.out.println("p2 to p3 " + p2.manhattanDistance(p3));
         System.out.println("p3 to p4 " + p3.manhattanDistance(p4));
 
+        System.out.println("Testing isCollinear: ");
+        System.out.println("p2, p4, p5: " + p2.isCollinear(p4,p5));
+        System.out.println("p2, p3, p4: " + p2.isCollinear(p3,p4));
+        System.out.println("p1, p2, p6: " + p2.isCollinear(p2,p6));
+        System.out.println("p1, p2, p3: " + p2.isCollinear(p2,p3));
+        
+
 
          
 
